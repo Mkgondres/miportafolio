7126a9093e714955cbb9c5a43070b3906015b2e3
Mkgondres <gondresmk@gmail.com>
2026-06-18 18:10:24 -0300

    Initialize Flask app with project rendering
    Set up a basic Flask application to render index.html with project data.

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from flask import Flask, render_template
+from proyectos import LISTA_PROYECTOS
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+    # Toma la lista de 'proyectos.py' y la envía al 'index.html'
+    return render_template("index.html", proyectos=LISTA_PROYECTOS)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
